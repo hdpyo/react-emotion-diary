23600a78aa3336594371c3ed53824f3bce997af2
hdpyo <vvhooping@gmail.com>
2022-07-26 18:19:25 +0900

    feat: Context 생성

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import './App.css';
 import MyButton from './components/MyButton';
 import MyHeader from './components/MyHeader';
 
+import { DiaryDispatchContext, DiaryStateContext } from './contexts';
+
 function reducer(state, action) {
   let newState = [];
 
@@ -80,35 +82,42 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader
-          headText="App"
-          leftChild={
-            <MyButton text="왼쪽 버튼" onClick={() => alert('왼쪽 클릭')} />
-          }
-          rightChild={
-            <MyButton text="오른쪽 버튼" onClick={() => alert('오른쪽 클릭')} />
-          }
-        />
-        <h1>Hello, React!</h1>
-        <MyButton
-          text="작성완료"
-          type="positive"
-          onClick={() => console.log('clicked!')}
-        />
-        <MyButton
-          text="삭제"
-          type="negative"
-          onClick={() => console.log('delete it!')}
-        />
-        <MyButton
-          text="버튼"
-          type="default"
-          onClick={() => console.log('nothing to do')}
-        />
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{ onCreate, onEdit, onRemove }}>
+        <BrowserRouter>
+          <div className="App">
+            <MyHeader
+              headText="App"
+              leftChild={
+                <MyButton text="왼쪽 버튼" onClick={() => alert('왼쪽 클릭')} />
+              }
+              rightChild={
+                <MyButton
+                  text="오른쪽 버튼"
+                  onClick={() => alert('오른쪽 클릭')}
+                />
+              }
+            />
+            <h1>Hello, React!</h1>
+            <MyButton
+              text="작성완료"
+              type="positive"
+              onClick={() => console.log('clicked!')}
+            />
+            <MyButton
+              text="삭제"
+              type="negative"
+              onClick={() => console.log('delete it!')}
+            />
+            <MyButton
+              text="버튼"
+              type="default"
+              onClick={() => console.log('nothing to do')}
+            />
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
