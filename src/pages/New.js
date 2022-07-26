3141852f906ca3de1491fd608f8c86e29acf1597
hdpyo <vvhooping@gmail.com>
2022-07-26 22:07:28 +0900

    feat: 날짜 선택 영역 추가

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,9 +1,18 @@
 import MyHeader from '../components/MyHeader';
 import MyButton from '../components/MyButton';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function New() {
   const navigate = useNavigate();
+
+  const getStringDate = date => {
+    // yyyy-MM-dd 형식으로 반환하기
+    return date.toISOString().slice(0, 10);
+  };
+
+  const [date, setDate] = useState(getStringDate(new Date()));
+
   return (
     <div>
       <MyHeader
@@ -12,6 +21,21 @@ function New() {
           <MyButton text={'< 뒤로 가기'} onClick={() => navigate(-1)} />
         }
       />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              type="date"
+              name=""
+              id=""
+              value={date}
+              onChange={e => setDate(e.target.value)}
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
