15a4c2a8be93bf6b3fc9deda8b546774b02d228e
hdpyo <vvhooping@gmail.com>
2022-07-26 18:00:33 +0900

    style: 공통 헤더 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
 
 function App() {
   return (
     <div className="App">
+      <MyHeader
+        headText="App"
+        leftChild={
+          <MyButton text="왼쪽 버튼" onClick={() => alert('왼쪽 클릭')} />
+        }
+        rightChild={
+          <MyButton text="오른쪽 버튼" onClick={() => alert('오른쪽 클릭')} />
+        }
+      />
       <h1>Hello, React!</h1>
       <MyButton
         text="작성완료"
---
+++ src/components/MyHeader.js
@@ -0,0 +1,9 @@
+export default function MyHeader({ headText, leftChild, rightChild }) {
+  return (
+    <header>
+      <div className="head_btn_left">{leftChild}</div>
+      <div className="head_text">{headText}</div>
+      <div className="head_btn_right">{rightChild}</div>
+    </header>
+  );
+}
