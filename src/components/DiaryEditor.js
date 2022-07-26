2f81610bec70530fe96487204742306524cefe20
hdpyo <vvhooping@gmail.com>
2022-07-26 22:11:56 +0900

    refactor: 공통 컴포넌트로 분리
    - 새 일기 작성과 일기 수정은 같은 구조를 갖기 때문에
    공통으로 사용할 컴포넌트인 DiaryEditor 로 분리한다.

---
+++ src/components/DiaryEditor.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import MyHeader from './MyHeader';
+import MyButton from './MyButton';
+
+function DiaryEditor() {
+  const navigate = useNavigate();
+
+  const getStringDate = date => {
+    // yyyy-MM-dd 형식으로 반환하기
+    return date.toISOString().slice(0, 10);
+  };
+
+  const [date, setDate] = useState(getStringDate(new Date()));
+
+  return (
+    <div>
+      <MyHeader
+        headText={'새 일기 쓰기'}
+        leftChild={
+          <MyButton text={'< 뒤로 가기'} onClick={() => navigate(-1)} />
+        }
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              type="date"
+              name=""
+              id=""
+              value={date}
+              onChange={e => setDate(e.target.value)}
+            />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default DiaryEditor;
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,41 +1,9 @@
-import MyHeader from '../components/MyHeader';
-import MyButton from '../components/MyButton';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import DiaryEditor from '../components/DiaryEditor';
 
 function New() {
-  const navigate = useNavigate();
-
-  const getStringDate = date => {
-    // yyyy-MM-dd 형식으로 반환하기
-    return date.toISOString().slice(0, 10);
-  };
-
-  const [date, setDate] = useState(getStringDate(new Date()));
-
   return (
     <div>
-      <MyHeader
-        headText={'새 일기 쓰기'}
-        leftChild={
-          <MyButton text={'< 뒤로 가기'} onClick={() => navigate(-1)} />
-        }
-      />
-      <div>
-        <section>
-          <h4>오늘은 언제인가요?</h4>
-          <div className="input-box">
-            <input
-              className="input-date"
-              type="date"
-              name=""
-              id=""
-              value={date}
-              onChange={e => setDate(e.target.value)}
-            />
-          </div>
-        </section>
-      </div>
+      <DiaryEditor />
     </div>
   );
 }
