b7d5e014af370c7e22d2c76d49ff397047e097b2
hdpyo <vvhooping@gmail.com>
2022-07-26 19:02:58 +0900

    feat: 선택한 연월에 해당하는 일기 목록 불러오기

---
+++ src/components/DiaryList.js
@@ -0,0 +1,15 @@
+function DiaryList({ diaryList }) {
+  return (
+    <div>
+      {diaryList.map(item => (
+        <div key={item.id}>{item.content}</div>
+      ))}
+    </div>
+  );
+}
+
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+
+export default DiaryList;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,45 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import MyHeader from '../components/MyHeader';
 import MyButton from '../components/MyButton';
 
+import { DiaryStateContext } from '../contexts';
+
+import DiaryList from '../components/DiaryList';
+
 export default function Home() {
+  const diaryList = useContext(DiaryStateContext);
+
+  const [data, setData] = useState([]);
+
   const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    // 선택한 달의 가장 첫 일, 말일을 구하기
+    if (diaryList.length < 1) {
+      return;
+    }
+
+    const firstDay = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      1,
+    ).getTime();
+
+    const lastDay = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth() + 1,
+      0,
+    ).getTime();
+
+    setData(
+      diaryList.filter(item => item.date >= firstDay && item.date <= lastDay),
+    );
+  }, [diaryList, currentDate]);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   const headText = `
     ${currentDate.getFullYear()}년 
@@ -37,6 +72,7 @@ export default function Home() {
         leftChild={<MyButton text={'<'} onClick={decreaseMonth} />}
         rightChild={<MyButton text={'>'} onClick={increaseMonth} />}
       />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
