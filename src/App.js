50fe859716229216d018de36b672d9b7944b2f45
hdpyo <vvhooping@gmail.com>
2022-07-26 18:05:34 +0900

    feat: BrowserRouter 적용

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import './App.css';
 import MyButton from './components/MyButton';
 import MyHeader from './components/MyHeader';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <MyHeader
-        headText="App"
-        leftChild={
-          <MyButton text="왼쪽 버튼" onClick={() => alert('왼쪽 클릭')} />
-        }
-        rightChild={
-          <MyButton text="오른쪽 버튼" onClick={() => alert('오른쪽 클릭')} />
-        }
-      />
-      <h1>Hello, React!</h1>
-      <MyButton
-        text="작성완료"
-        type="positive"
-        onClick={() => console.log('clicked!')}
-      />
-      <MyButton
-        text="삭제"
-        type="negative"
-        onClick={() => console.log('delete it!')}
-      />
-      <MyButton
-        text="버튼"
-        type="default"
-        onClick={() => console.log('nothing to do')}
-      />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <MyHeader
+          headText="App"
+          leftChild={
+            <MyButton text="왼쪽 버튼" onClick={() => alert('왼쪽 클릭')} />
+          }
+          rightChild={
+            <MyButton text="오른쪽 버튼" onClick={() => alert('오른쪽 클릭')} />
+          }
+        />
+        <h1>Hello, React!</h1>
+        <MyButton
+          text="작성완료"
+          type="positive"
+          onClick={() => console.log('clicked!')}
+        />
+        <MyButton
+          text="삭제"
+          type="negative"
+          onClick={() => console.log('delete it!')}
+        />
+        <MyButton
+          text="버튼"
+          type="default"
+          onClick={() => console.log('nothing to do')}
+        />
+      </div>
+    </BrowserRouter>
   );
 }
 
