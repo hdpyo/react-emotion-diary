ff904f7a9eeb274af0ad71f19bfb05a39fe20990
hdpyo <vvhooping@gmail.com>
2022-07-26 20:47:39 +0900

    feat: 최신순/오래된 순으로 일기 정렬하기

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,7 +1,49 @@
+import { useState } from 'react';
+
+const sortOptionList = [
+  { id: '1', value: 'latest', name: '최신순' },
+  { id: '2', value: 'oldest', name: '오래된 순' },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={e => onChange(e.target.value)}>
+      {optionList.map(item => (
+        // eslint-disable-next-line react/jsx-key
+        <option key={item.id} value={item.value}>
+          {item.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
 function DiaryList({ diaryList }) {
+  const [sortType, setSortType] = useState('latest');
+
+  const getProcessedDiaryList = () => {
+    // 최신순인지 오래된순인지 비교하는 함수
+    const compare = (a, b) => {
+      if (sortType === 'latest') {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+
+    // 배열 깊은 복사
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    return copyList.sort(compare);
+  };
+
   return (
     <div>
-      {diaryList.map(item => (
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
+      {getProcessedDiaryList().map(item => (
         <div key={item.id}>{item.content}</div>
       ))}
     </div>
